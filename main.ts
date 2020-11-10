ff7c00fbb84dbdf45484903e08ee21c233e2bf29
tonyspm <73495998+tonyspm@users.noreply.github.com>
2020-11-10 15:02:07 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        basic.pause(1000)
+    }
+    basic.clearScreen()
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        basic.pause(1000)
+    }
+    basic.clearScreen()
+    if (input.buttonIsPressed(Button.A) && input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+})
