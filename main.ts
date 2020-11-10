fd0e0e20023ccd71fad1acfa179d596d5e83f2e9
tonyspm <73495998+tonyspm@users.noreply.github.com>
2020-11-10 15:44:17 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+})
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         basic.showLeds(`
